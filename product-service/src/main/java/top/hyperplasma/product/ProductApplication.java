fe4b4572b254d724e93f36574eeb6dae711f33cf
Akira37 <863781751@qq.com>
2025-01-16 23:26:50 +0800

    Add starter

---
+++ product-service/src/main/java/top/hyperplasma/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package top.hyperplasma.product;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("top.hyperplasma.product.mapper")
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
