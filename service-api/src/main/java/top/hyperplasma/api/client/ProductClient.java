f44c785b3aabbc01b782a60683545e56e121ce37
Akira37 <863781751@qq.com>
2025-01-20 19:30:04 +0800

    Update Product Client

--- service-api/src/main/java/top/hyperplasma/api/client/ProductClient.java
+++ service-api/src/main/java/top/hyperplasma/api/client/ProductClient.java
@@ -1,4 +1,22 @@
 package top.hyperplasma.api.client;
 
-public class ProductClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.hyperplasma.api.client.fallback.ProductClientFallbackFactory;
+import top.hyperplasma.api.dto.OrderDetailDTO;
+import top.hyperplasma.api.dto.ProductDTO;
+
+import java.util.Collection;
+import java.util.List;
+
+@FeignClient(value = "product-service", fallbackFactory = ProductClientFallbackFactory.class)
+public interface ProductClient {
+    @GetMapping("/products")
+    List<ProductDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @PutMapping("/products/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> products);
 }
--- service-api/src/main/java/top/hyperplasma/api/client/fallback/ProductClientFallbackFactory.java
+++ service-api/src/main/java/top/hyperplasma/api/client/fallback/ProductClientFallbackFactory.java
@@ -1,11 +1,33 @@
 package top.hyperplasma.api.client.fallback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import top.hyperplasma.api.client.ProductClient;
+import top.hyperplasma.api.dto.OrderDetailDTO;
+import top.hyperplasma.api.dto.ProductDTO;
+import top.hyperplasma.common.utils.CollUtils;
 
+import java.util.Collection;
+import java.util.List;
+
+@Slf4j
 public class ProductClientFallbackFactory implements FallbackFactory<ProductClient> {
+
     @Override
     public ProductClient create(Throwable cause) {
-        return null;
+        return new ProductClient() {
+            @Override
+            public List<ProductDTO> queryItemByIds(Collection<Long> ids) {
+                log.error("查询商品失败！", cause);
+                return CollUtils.emptyList();
+            }
+
+            @Override
+            public void deductStock(List<OrderDetailDTO> items) {
+                log.error("扣减商品库存失败！", cause);
+                throw new RuntimeException(cause);
+            }
+        };
     }
+
 }
